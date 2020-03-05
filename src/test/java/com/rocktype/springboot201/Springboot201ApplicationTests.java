2a64916743c1d0f45a1882df9982ce17c48dba65
GBTC440013UR <ryanmurphy2014@gmail.com>
2020-03-04 20:39:34 -0500

    Initial commit

---
+++ src/test/java/com/rocktype/springboot201/Springboot201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocktype.springboot201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
